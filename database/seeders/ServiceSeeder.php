7a3ea29c25099e81f10543bdcdbf76874b9b594a
asmi046 <asmi046@gmail.com>
2025-07-11 21:28:04 +0300

    Сидер который надо заполнить

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use DB;
+
 class ServiceSeeder extends Seeder
 {
     /**
@@ -12,6 +14,156 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+
+            [
+                'title' => "Строительство бань",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Шлифовка бань и домов из сруба",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Кровельные работы",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Внутренняя отделка",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Строительство веранд и террасс",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Строительство беседок",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Ленточный фундамент",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Свайный фундамент",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Монолитный фундамент",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Строительство заборов",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Установка откатных и распашных ворот",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+            [
+                'title' => "Строительство навесов",
+                'template' => null,
+                'img' => 'service/banya.webp',
+                'slug'=> Str::slug("Строительство бань"),
+                'short_description' => file_get_contents(public_path('tmp_data/serv/s_1/short.html')),
+                'description' => file_get_contents(public_path('tmp_data/serv/s_1/main.html')),
+                'gallery' => null,
+                'sections' => null,
+            ],
+
+
+
+        ];
+
+
+        foreach ($data as $item) {
+            // Storage::disk('public')->put("main_bnr.webp", file_get_contents(public_path('img/main_bnr.webp')), 'public');
+            DB::table("services")->insert($item);
+
+            // DB::table("seo_data")->insert(
+            //     [
+            //         'url' => 'page/'.$item['slug'],
+            //         'seo_title' => $item['title'],
+            //         'seo_description' => $item['title'],
+            //     ]
+            // );
+        }
     }
 }
