264c30d6286283f85aa36671411a501cda4f75ef
asmi046 <asmi046@gmail.com>
2025-07-20 10:51:42 +0300

    Вывод продуктов на страницу

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,6 +8,12 @@
                 <h2 class="section__title __left">Проекты домов</h2>
                 <p class="section__subtitle __left">Представляем Вашему вниманию каталог проектов домов доступных для заказа</p>
             </div>
+
+            <div class="projects-section__grid">
+                @foreach ($projects as $item)
+                    <x-cards.project :item="$item" />
+                @endforeach
+            </div>
         </div>
     </section>
 
