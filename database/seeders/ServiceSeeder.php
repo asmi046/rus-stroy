7d06c90f4a7c9b08fee746478f27bd2cf693d85e
Hitman1042 <158605565+Hitman1042@users.noreply.github.com>
2025-07-27 15:39:47 +0300

    seviceseeder фото

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -46,6 +46,63 @@ class ServiceSeeder extends Seeder
         Storage::disk('public')->put("service/s3_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_3/s3_gal_2.webp')), 'public');
         Storage::disk('public')->put("service/s3_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_3/s3_gal_3.jpg')), 'public');
 
+        Storage::disk('public')->put("service/s4_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_4/s4_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s4_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_4/s4_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s4_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_4/s4_gal_3.jpg')), 'public');
+        Storage::disk('public')->put("service/s4_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_4/s4_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s5_gal_1.jpg", file_get_contents(public_path('tmp_data/serv/s_5/s5_gal_1.jpg')), 'public');
+        Storage::disk('public')->put("service/s5_gal_2.jpg", file_get_contents(public_path('tmp_data/serv/s_5/s5_gal_2.jpg')), 'public');
+        Storage::disk('public')->put("service/s5_gal_3.webp", file_get_contents(public_path('tmp_data/serv/s_5/s5_gal_3.webp')), 'public');
+        Storage::disk('public')->put("service/s5_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_5/s5_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s6_gal_1.jpg", file_get_contents(public_path('tmp_data/serv/s_6/s6_gal_1.jpg')), 'public');
+        Storage::disk('public')->put("service/s6_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_6/s6_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s6_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_6/s6_gal_3.jpg')), 'public');
+        Storage::disk('public')->put("service/s6_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_6/s6_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s7_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_7/s7_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s7_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_7/s7_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s7_gal_3.webp", file_get_contents(public_path('tmp_data/serv/s_7/s7_gal_3.webp')), 'public');
+        Storage::disk('public')->put("service/s7_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_7/s7_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s8_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_8/s8_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s8_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_8/s8_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s8_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_8/s8_gal_3.jpg')), 'public');
+        Storage::disk('public')->put("service/s8_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_8/s8_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s9_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_9/s9_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s9_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_9/s9_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s9_gal_3.webp", file_get_contents(public_path('tmp_data/serv/s_9/s9_gal_3.webp')), 'public');
+        Storage::disk('public')->put("service/s9_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_9/s9_gal_4.jpg')), 'public');
+        Storage::disk('public')->put("service/s9_gal_5.webp", file_get_contents(public_path('tmp_data/serv/s_9/s9_gal_5.webp')), 'public');
+
+        Storage::disk('public')->put("service/s10_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_10/s10_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s10_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_10/s10_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s10_gal_3.webp", file_get_contents(public_path('tmp_data/serv/s_10/s10_gal_3.webp')), 'public');
+        Storage::disk('public')->put("service/s10_gal_4.webp", file_get_contents(public_path('tmp_data/serv/s_10/s10_gal_4.webp')), 'public');
+        Storage::disk('public')->put("service/s10_gal_5.jpg", file_get_contents(public_path('tmp_data/serv/s_10/s10_gal_5.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s11_gal_1.jpg", file_get_contents(public_path('tmp_data/serv/s_11/s11_gal_1.jpg')), 'public');
+        Storage::disk('public')->put("service/s11_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_11/s11_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s11_gal_3.webp", file_get_contents(public_path('tmp_data/serv/s_11/s11_gal_3.webp')), 'public');
+        Storage::disk('public')->put("service/s11_gal_4.webp", file_get_contents(public_path('tmp_data/serv/s_11/s11_gal_4.webp')), 'public');
+        Storage::disk('public')->put("service/s11_gal_5.webp", file_get_contents(public_path('tmp_data/serv/s_11/s11_gal_5.webp')), 'public');
+
+        Storage::disk('public')->put("service/s12_gal_1.webp", file_get_contents(public_path('tmp_data/serv/s_12/s12_gal_1.webp')), 'public');
+        Storage::disk('public')->put("service/s12_gal_2.webp", file_get_contents(public_path('tmp_data/serv/s_12/s12_gal_2.webp')), 'public');
+        Storage::disk('public')->put("service/s12_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_12/s12_gal_3.jpg')), 'public');
+        Storage::disk('public')->put("service/s12_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_12/s12_gal_4.jpg')), 'public');
+
+        Storage::disk('public')->put("service/s13_gal_1.jpg", file_get_contents(public_path('tmp_data/serv/s_13/s13_gal_1.jpg')), 'public');
+        Storage::disk('public')->put("service/s13_gal_2.jpg", file_get_contents(public_path('tmp_data/serv/s_13/s13_gal_2.jpg')), 'public');
+        Storage::disk('public')->put("service/s13_gal_3.jpg", file_get_contents(public_path('tmp_data/serv/s_13/s13_gal_3.jpg')), 'public');
+        Storage::disk('public')->put("service/s13_gal_4.jpg", file_get_contents(public_path('tmp_data/serv/s_13/s13_gal_4.jpg')), 'public');
+        Storage::disk('public')->put("service/s13_gal_5.jpg", file_get_contents(public_path('<tmp_data>
+        <serv>
+        <s_13>
+        <s13></s13>_gal_5.jpg')), 'public');
+
         $data = [
 
             [
