eb146ca6450ff3d8ce31d9aa3c81dd4822287928
asmi046 <asmi046@gmail.com>
2025-10-22 18:10:25 +0300

    Опечатка и материалы

--- resources/views/components/cta-block.blade.php
+++ resources/views/components/cta-block.blade.php
@@ -32,7 +32,7 @@
                 <img src="{{ asset('img/manager.webp') }}" alt="Менеджер" class="photo__img">
                 <div class="beeper"></div>
             </div>
-            <p class="contact-block__text">Отвкечу на все Ваши вопросы, помогу определиться с выбором проекта и бюджетом</p>
+            <p class="contact-block__text">Отвечу на все Ваши вопросы, помогу определиться с выбором проекта и бюджетом</p>
             <a href="tel:+7{{ phone_format(get_city_text('[city|phone_f]')) }}" class="contact-block__phone">{{ get_city_text('[city|phone]') }}</a>
         </div>
     </div>
