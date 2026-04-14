66346f936c1e868a7c1858d54f37688b67b5ad08
asmi046 <asmi046@gmail.com>
2026-04-14 14:55:00 +0300

    Правки

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use DB;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class MenuSeeder extends Seeder
 {
@@ -15,227 +13,227 @@ class MenuSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            [
-                'menu_name' => 'Главное меню',
-                'title' => 'О компании',
-                'order' => 1,
-                'lnk' => "/page/o-kompanii-russtroi",
-            ],
+
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Оплата строительства',
                 'order' => 2,
-                'lnk' => "/page/sposoby-oplaty",
+                'lnk' => '/page/sposoby-oplaty',
             ],
 
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Строительство',
                 'order' => 3,
-                'lnk' => "/building",
+                'lnk' => '/building',
             ],
 
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Услуги',
                 'order' => 3,
-                'lnk' => "/services",
+                'lnk' => '/services',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Проекты домов',
                 'order' => 4,
-                'lnk' => "/projects/doma",
+                'lnk' => '/projects/doma',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Проекты бань',
                 'order' => 4,
-                'lnk' => "/projects/bani",
+                'lnk' => '/projects/bani',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Портфолио',
                 'order' => 5,
-                'lnk' => "/portfolio",
+                'lnk' => '/portfolio',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Цены',
                 'order' => 6,
-                'lnk' => "/prices",
+                'lnk' => '/prices',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'FAQ',
                 'order' => 7,
-                'lnk' => "/faq",
+                'lnk' => '/faq',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Избранные',
                 'order' => 7,
-                'lnk' => "/like",
+                'lnk' => '/like',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Сравнение проектов',
                 'order' => 7,
-                'lnk' => "/compare",
+                'lnk' => '/compare',
             ],
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Ипотека',
                 'order' => 8,
-                'lnk' => "/page/ipoteka-na-stroitelstvo-domov",
+                'lnk' => '/page/ipoteka-na-stroitelstvo-domov',
             ],
 
             [
                 'menu_name' => 'Главное меню',
                 'title' => 'Контакты',
                 'order' => 9,
-                'lnk' => "/contacts",
+                'lnk' => '/contacts',
+            ],
+
+            [
+                'menu_name' => 'Главное меню',
+                'title' => 'О компании',
+                'order' => 10,
+                'lnk' => '/page/o-kompanii-russtroi',
             ],
         ];
 
-        DB::table("menus")->insert($data);
+        DB::table('menus')->insert($data);
 
         $data = [
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'О компании',
-                'order' => 1,
-                'lnk' => "/page/o-kompanii-russtroi",
+                'order' => 10,
+                'lnk' => '/page/o-kompanii-russtroi',
             ],
-
 
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Строительство',
                 'order' => 3,
-                'lnk' => "/building",
+                'lnk' => '/building',
             ],
 
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Услуги',
                 'order' => 3,
-                'lnk' => "/services",
+                'lnk' => '/services',
             ],
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Проекты домов',
                 'order' => 4,
-                'lnk' => "/projects/doma",
+                'lnk' => '/projects/doma',
             ],
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Проекты бань',
                 'order' => 4,
-                'lnk' => "/projects/bani",
+                'lnk' => '/projects/bani',
             ],
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Портфолио',
                 'order' => 5,
-                'lnk' => "/portfolio",
+                'lnk' => '/portfolio',
             ],
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Цены',
                 'order' => 6,
-                'lnk' => "/prices",
+                'lnk' => '/prices',
             ],
 
             [
                 'menu_name' => 'Главное меню в шапке',
                 'title' => 'Контакты',
                 'order' => 9,
-                'lnk' => "/contacts",
+                'lnk' => '/contacts',
             ],
         ];
 
-        DB::table("menus")->insert($data);
+        DB::table('menus')->insert($data);
 
         $footer_menu = [
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Фасадные работы',
                 'order' => 1,
-                'lnk' => "/services/fasadnye-raboty",
+                'lnk' => '/services/fasadnye-raboty',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Строительство бань',
                 'order' => 2,
-                'lnk' => "/services/stroitelstvo-ban-iz-brevna",
+                'lnk' => '/services/stroitelstvo-ban-iz-brevna',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Сливовка бань и домов',
                 'order' => 3,
-                'lnk' => "/services/slifovka-ban-i-domov",
+                'lnk' => '/services/slifovka-ban-i-domov',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Кровельные работы',
                 'order' => 4,
-                'lnk' => "/services/krovelnye-raboty",
+                'lnk' => '/services/krovelnye-raboty',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Внутренняя отделка',
                 'order' => 7,
-                'lnk' => "/services/vnutrenniaia-otdelka",
+                'lnk' => '/services/vnutrenniaia-otdelka',
             ],
 
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Строительство веранд и террас',
                 'order' => 7,
-                'lnk' => "/services/stroitelstvo-verand-i-terrass",
+                'lnk' => '/services/stroitelstvo-verand-i-terrass',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Строительство беседок',
                 'order' => 7,
-                'lnk' => "/services/stroitelstvo-besedok",
+                'lnk' => '/services/stroitelstvo-besedok',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Ленточный фундамент',
                 'order' => 7,
-                'lnk' => "/services/lentocnyi-fundament",
+                'lnk' => '/services/lentocnyi-fundament',
             ],
 
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Свайный фундамент',
                 'order' => 7,
-                'lnk' => "/services/svainyi-fundament",
+                'lnk' => '/services/svainyi-fundament',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Монолитный фундамент',
                 'order' => 7,
-                'lnk' => "/services/monolitnyi-fundament",
+                'lnk' => '/services/monolitnyi-fundament',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Строительство заборов',
                 'order' => 7,
-                'lnk' => "/services/stroitelstvo-zaborov",
+                'lnk' => '/services/stroitelstvo-zaborov',
             ],
             [
                 'menu_name' => 'Услуги в подвале',
                 'title' => 'Строительство навесов',
                 'order' => 7,
-                'lnk' => "/services/stroitelstvo-navesov",
+                'lnk' => '/services/stroitelstvo-navesov',
             ],
         ];
 
-        DB::table("menus")->insert($footer_menu);
-
+        DB::table('menus')->insert($footer_menu);
 
         $footer_menu_2 = [
 
@@ -243,23 +241,23 @@ class MenuSeeder extends Seeder
                 'menu_name' => 'Проекты в подвале',
                 'title' => 'Из газоблока',
                 'order' => 10,
-                'lnk' => "/materials/doma-iz-gazoblokov",
+                'lnk' => '/materials/doma-iz-gazoblokov',
             ],
             [
                 'menu_name' => 'Проекты в подвале',
                 'title' => 'Из бруса',
                 'order' => 20,
-                'lnk' => "/materials/doma-iz-brusa",
+                'lnk' => '/materials/doma-iz-brusa',
             ],
             [
                 'menu_name' => 'Проекты в подвале',
                 'title' => 'Из профилированного бруса',
                 'order' => 30,
-                'lnk' => "/materials/doma-iz-profilirovannogo-brusa",
+                'lnk' => '/materials/doma-iz-profilirovannogo-brusa',
             ],
         ];
 
-        DB::table("menus")->insert($footer_menu_2);
+        DB::table('menus')->insert($footer_menu_2);
 
         $footer_menu_3 = [
 
@@ -267,36 +265,36 @@ class MenuSeeder extends Seeder
                 'menu_name' => 'О компании в подвале',
                 'title' => 'О нкас',
                 'order' => 10,
-                'lnk' => "/page/o-kompanii-russtroi",
+                'lnk' => '/page/o-kompanii-russtroi',
             ],
             [
                 'menu_name' => 'О компании в подвале',
                 'title' => 'Способы оплаты',
                 'order' => 20,
-                'lnk' => "/page/sposoby-oplaty",
+                'lnk' => '/page/sposoby-oplaty',
             ],
             [
                 'menu_name' => 'О компании в подвале',
                 'title' => 'Портфолио',
                 'order' => 30,
-                'lnk' => "/portfolio",
+                'lnk' => '/portfolio',
             ],
             [
                 'menu_name' => 'О компании в подвале',
                 'title' => 'Цены',
                 'order' => 40,
-                'lnk' => "/prices",
+                'lnk' => '/prices',
             ],
             [
                 'menu_name' => 'О компании в подвале',
                 'title' => 'Контакты',
                 'order' => 40,
-                'lnk' => "/contacts",
+                'lnk' => '/contacts',
             ],
 
         ];
 
-        DB::table("menus")->insert($footer_menu_3);
+        DB::table('menus')->insert($footer_menu_3);
 
         $footer_menu_4 = [
 
@@ -304,23 +302,23 @@ class MenuSeeder extends Seeder
                 'menu_name' => 'Информация в подвале',
                 'title' => 'Политика конфиденциальности и обработки персональных данных',
                 'order' => 10,
-                'lnk' => "/page/politika-v-oblasti-obrabotki-personalnyx-dannyx",
+                'lnk' => '/page/politika-v-oblasti-obrabotki-personalnyx-dannyx',
             ],
             [
                 'menu_name' => 'Информация в подвале',
                 'title' => 'Согласие на обработку персональных данных',
                 'order' => 20,
-                'lnk' => "/page/soglasie-na-obrabotku-personalnyx-dannyx",
+                'lnk' => '/page/soglasie-na-obrabotku-personalnyx-dannyx',
             ],
             [
                 'menu_name' => 'Информация в подвале',
                 'title' => 'FAQ',
                 'order' => 30,
-                'lnk' => "/faq",
+                'lnk' => '/faq',
             ],
 
         ];
 
-        DB::table("menus")->insert($footer_menu_4);
+        DB::table('menus')->insert($footer_menu_4);
     }
 }
